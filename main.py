02f3f5ad2bf276e40df0c6da398b6eead00b2dae
Zerex1 <dimaomorokov@gmail.com>
2022-02-24 06:08:52 +0500

    #1 Сделали бот выводящий сталицы стран

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def get_text_massage(message):
-    username = message.from_user.username
-    msg = f'Кто здесь? {username}'
-    bot.send_message(message.from_user.id, msg)
+    filename = 'Stolica.csv'
+    open(filename).readline().split('\n')
+    portfolio = []
+    for line in open(filename):
+        fields = line.split(",")
+        country = fields[0]
+        capital = fields[1]
+        capital = capital.rstrip("\n")
+        stock = (country, capital)
+        portfolio.append(stock)
+
+    for i in range(0, len(portfolio)):
+        for j in range(0, len(portfolio[i])):
+            if message.text == portfolio[i][0]:
+                bot.send_message(message.from_user.id, portfolio[i][1])
+                bot.send_message(message.from_user.id, "Введите страну")
+                return
+            else:
+                if i == len(portfolio)-1:
+                    bot.send_message(message.from_user.id, "Нет такой страны, попробуйте заново")
+                    return
+
 
 
 bot.polling()
